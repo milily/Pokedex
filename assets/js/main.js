c8a39b4282e6fc294ba2e2eb60f34a60d5703f10
mily rivas <mrise.translations@gmail.com>
2017-07-10 00:57:38 -0400

    agregando funcionalidad al cargar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,30 @@
-$(document).ready(function() {
-
-    $(".loader").hide(); //Ocultamos el loader por defecto
-    $(".loader2").hide(); //Ocultamos el loader por defecto
-
+$(document).ready(function(){
+   
+   $('#loader1').hide(); //ocultamos el loader por defecto.
+   $('#loader2').hide(); //ocultamos el modal por defecto.
+   
     var imagenes_disponibles = ['015','024','059','063','065','142','144',
                                 '153','168','181','182','184','190','267','298',
                                 '304','306','334','339','343','347','348','354',
                                 '371','374','411','424','482','493','531','550','566',
                                 '567','591','594','606','610','614','617','681','683',
                                 '689','698','713','752']
-
-    listarTodo(imagenes_disponibles)
-
-    $("body").on('click','.pokemon', function(){
-        var pokemon_id = $(this).attr("id")
-
-        buscarPokemon(pokemon_id)
-    });
     
+    
+    function buscar_todo(){
+        $.ajax({
+            url: 'http://pokeapi.co/api/v2/pokedex/1/',
+            type: 'GET',
+            dataType: 'JSON',
+            data: { 'limit':'811'},
+            beforeSend: function(){
+                $("#loader1").show();
+            }
+        })
+        .done( function(data){
+            console.log(data)
+        })
+    }
+    buscar_todo();
 });
+
